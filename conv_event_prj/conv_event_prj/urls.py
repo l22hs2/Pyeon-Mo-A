ae64ca7d16ad9e96c96066b4b92f285cba6e2aeb
l22hs <l22hs2@gmail.com>
2022-06-06 20:33:25 +0900

    댓글 기능 추가 전 작업

--- conv_event_prj/conv_event_prj/urls.py
+++ conv_event_prj/conv_event_prj/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', views.home),
+    path('', views.home, name="home"),
     path('event/', include("web.urls")),
 
     path('accounts/', include('allauth.urls'), name="google"), 
